f40be492d6994c4c64bac1c0d6c7b913f929bc38
Giovanni Rufino <giovannirufino@gmail.com>
2022-01-09 20:30:40 -0500

    refactored target over array values code in findFloor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,22 @@
-function findFloor() {
-  
+function findFloor(arr, target, low = 0, high = arr.length - 1) {
+  if (target < arr[0]) return -1;
+  if (target >= arr[high]) return arr[high];
+
+  while (high >= low) {
+    let midIdx = Math.floor((high + low) / 2);
+
+    if (arr[midIdx] === target) return arr[midIdx];
+
+    if (arr[midIdx - 1] <= target && target < arr[midIdx]) {
+      return arr[midIdx - 1];
+    } else if (target < arr[midIdx]) {
+      high = midIdx - 1;
+    } else {
+      low = midIdx + 1;
+    }
+  }
+
+  return -1;
 }
 
-module.exports = findFloor
+module.exports = findFloor;
--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,19 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  if (arr.length === 0 || arr[0] < arr[arr.length - 1]) {
+    return 0;
+  }
+  let lowIdx = 0;
+  let highIdx = arr.length - 1;
+  while (lowIdx <= highIdx) {
+    let midIdx = Math.floor((highIdx + lowIdx) / 2);
+    if (arr[midIdx] > arr[midIdx + 1]) {
+      return midIdx + 1;
+    } else if (arr[lowIdx] <= arr[midIdx]) {
+      lowIdx = midIdx + 1;
+    } else {
+      highIdx = midIdx - 1;
+    }
+  }
 }
 
-module.exports = findRotationCount
+module.exports = findRotationCount;
